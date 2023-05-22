ae6385ef6b305559e2900a940ef43481ad448d93
Pino Ricci <pino.ricci@she.net>
2023-05-22 18:28:57 +0200

    Initial Commit

---
+++ client/main.js
@@ -0,0 +1,75 @@
+class Poll {
+    constructor(root, title, postUrl, cookieName) {
+        this.root = root;
+        this.cookieName = cookieName;
+        this.selected = sessionStorage.getItem(cookieName);
+        this.endpoint = "http://localhost:3000/" + postUrl;
+
+        this.root.insertAdjacentHTML("afterbegin", `
+            <div class="poll__title">${ title }</div>
+        `);
+
+        this._refresh();
+    }
+
+    async _refresh() {
+        const response = await fetch(this.endpoint);
+        const data = await response.json();
+
+        this.root.querySelectorAll(".poll__option").forEach(option => {
+            option.remove();
+        });
+
+        for (const option of data) {
+            const template = document.createElement("template");
+            const fragment = template.content;
+
+            template.innerHTML = `
+                <div class="poll__option ${ this.selected == option.label ? "poll__option--selected": "" }">
+                    <div class="poll__option-fill"></div>
+                    <div class="poll__option-info">
+                        <span class="poll__label">${ option.label }</span>
+                        <span class="poll__percentage">${ option.percentage }%</span>
+                    </div>
+                </div>
+            `;
+
+            if (!this.selected) {
+                fragment.querySelector(".poll__option").addEventListener("click", () => {
+                    fetch(this.endpoint, {
+                        method: "post",
+                        body: `add=${ option.label }`,
+                        headers: {
+                            "Content-Type": "application/x-www-form-urlencoded"
+                        }
+                    }).then(() => {
+                        this.selected = option.label;
+
+                        sessionStorage.setItem(this.cookieName, option.label);
+
+                        this._refresh();
+                    })
+                });
+            }
+
+            fragment.querySelector(".poll__option-fill").style.width = `${ option.percentage }%`;
+
+            this.root.appendChild(fragment);
+        }
+    }
+}
+
+const p = new Poll(
+    document.querySelector(".reDelBallo"),
+    "Vota il re del ballo!",
+    "poll",
+    "reDelBalloCookie"
+);
+
+
+const reginetta = new Poll(
+    document.querySelector(".reginettaDelBallo"),
+    "Vota la reginetta del ballo!",
+    "reginetta",
+    "reginettaDelBalloCookie"
+);
---
+++ server/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const fs = require("fs").promises;
+const path = require("path");
+
+const app = express();
+const dataFile = path.join(__dirname, "data.json");
+const reginettaFile = path.join(__dirname, "reginetta.json");
+
+// Support POSTing form data with URL encoded
+app.use(express.urlencoded({ extended: true }));
+
+// Enable CORS
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    next();
+});
+
+app.get("/poll", async (req, res) => {
+    getManager(req, res, dataFile);
+});
+
+app.get("/reginetta", async (req, res) => {
+    getManager(req, res, reginettaFile);
+});
+
+app.post("/poll", async (req, res) => {
+    postManager(req, res, dataFile);
+});
+
+app.post("/reginetta", async (req, res) => {
+    postManager(req, res, reginettaFile);
+});
+
+app.listen(3000, () => console.log("Server is running..."));
+
+
+
+
+
+async function postManager(req, res, file){
+    const data = JSON.parse(await fs.readFile(file, "utf-8"));
+
+    data[req.body.add]++;
+
+    await fs.writeFile(file, JSON.stringify(data));
+
+    res.end();
+}
+
+async function getManager(req, res, file){
+    let data = JSON.parse(await fs.readFile(file, "utf-8"));
+    const totalVotes = Object.values(data).reduce((total, n) => total += n, 0);
+
+    data = Object.entries(data).map(([label, votes]) => {
+        return {
+            label,
+            percentage: (((100 * votes) / totalVotes) || 0).toFixed(0)
+        }
+    });
+
+    res.json(data);
+}
